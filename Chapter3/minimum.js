8ea3ee3593c770e83c88361a381900918307f3ac
Justin Hiltz <justin@justinhiltz.com>
2016-04-08 12:30:31 -0400

    Initial commit

---
+++ Chapter3/minimum.js
@@ -0,0 +1,11 @@
+// Write a function that takes two arguments and returns their minimum.
+
+function min(num1, num2) {
+  if (num1 > num2) {
+    return num2;
+  } else {
+    return num1;
+  };
+}
+
+min(2, 34);
